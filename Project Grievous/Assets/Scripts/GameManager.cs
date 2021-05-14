cc077590f4a8139dec43c8c33419d421d55ca743
Tigo <tiago.delgado@tecnico.ulisboa.pt>
2021-05-14 19:32:41 +0100

    moving objects, wall layer (need to fix level scene)

--- Project Grievous/Assets/Scripts/GameManager.cs
+++ Project Grievous/Assets/Scripts/GameManager.cs
@@ -64,7 +64,7 @@ public class GameManager : MonoBehaviour
 
     private void HandleStartState()
     {
-        //FIXME
+        Timer.Instance?.ResetTimer();
         UpdateGameState(GameState.Running);
     }
 
